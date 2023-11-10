8ec0cc06a5bbd53f69e570ea62661ffea5575646
example <example@gmail.com>
2023-11-10 18:58:14 +0500

    js file junatildi

---
+++ script.js
@@ -0,0 +1,31 @@
+// Git and GitHub (VCS => version control sysytem);
+// GIT => Local version control system
+// GitHub => online version control system
+
+
+// git config --list => configurationlar ruyxati
+// git config --global user.name "user namingiz"
+// git config --global user.email "user emailingiz"
+// git init => project ni git nazoratiga quyish, faqat boshida bir marta beriladi.
+// git status => uzgarish bor yuqligini tekshirish
+// touch fileName => fayl yaratish
+// rm fileName => file ni uchirish
+// git add fileName => bulgan uzgarishni navbatga quyadi
+// git commit -m "bulgan uzgarish haqida comment" => tuliq git ga saxranit qiladi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// windows => gitBash (terminal)
+// MacOs => iTerm, Kitty
